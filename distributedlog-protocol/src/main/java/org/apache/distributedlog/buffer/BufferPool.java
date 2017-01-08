ae8e1ab40ecca41fe9c80a0b4f3bdb39a134a7c4
Sijie Guo <sijieg@twitter.com>
2017-01-07 17:27:20 -0800

    Fix

--- distributedlog-protocol/src/main/java/org/apache/distributedlog/buffer/BufferPool.java
+++ distributedlog-protocol/src/main/java/org/apache/distributedlog/buffer/BufferPool.java
@@ -1,20 +1,30 @@
 package org.apache.distributedlog.buffer;
 
 import io.netty.buffer.PooledByteBufAllocator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Buffer Pool.
  */
 public class BufferPool {
 
+    private static final Logger logger = LoggerFactory.getLogger(BufferPool.class);
+
     public static final PooledByteBufAllocator ALLOCATOR;
 
     static {
+        logger.info("Initializing buffer pool ...");
         int pageSize;
         try {
-            pageSize = Integer.parseInt(System.getProperty("dl.buffer.allocator.pageSize", "8192"));
-        } catch (NumberFormatException nfe) {
-            pageSize = 8192;
+            try {
+                pageSize = Integer.parseInt(System.getProperty("dl.buffer.allocator.pageSize", "8192"));
+            } catch (NumberFormatException nfe) {
+                pageSize = 8192;
+            }
+        } catch (Throwable t) {
+            logger.info("Encountered exception on initializing buffer pool", t);
+            throw t;
         }
         ALLOCATOR = new PooledByteBufAllocator(
                 true,   // prefer direct
@@ -26,5 +36,6 @@ public class BufferPool {
                 32,     // smallCacheSize
                 8       // normalCacheSize
         );
+        logger.info("Initialized buffer pool.");
     }
 }
